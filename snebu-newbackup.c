384c658615b5e2cbd8c6dde83efa02a04ec7fcb7
Derek Pressnall <derekp@homesvr1>
2021-01-08 14:30:05 -0600

    Updated copyright notices

--- snebu-newbackup.c
+++ snebu-newbackup.c
@@ -1,3 +1,21 @@
+/* Copyright 2009 - 2021 Derek Pressnall
+ *
+ * This file is part of Snebu, the Simple Network Encrypting Backup Utility
+ *
+ * Snebu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 3
+ * as published by the Free Software Foundation.
+ *
+ * Snebu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Snebu.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
